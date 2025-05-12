297edabd7de69a00c99799e01f531dd27bd3afc8
Jawad <Jawadsadiq7373@gmail.com>
2025-05-12 12:18:54 -0700

    Starter files for this project created

---
+++ src/main/java/com/university/sms/controller/Student.java
@@ -0,0 +1,11 @@
+package com.university.sms.controller;
+
+public class Student {
+
+
+    private Long id;
+    private String firstName;
+    private String LastName;
+    private String email;
+    private String department;
+}
