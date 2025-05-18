af3f85eb3a9409dd80c28f14b938912ebb3f0cf3
Jawad <Jawadsadiq7373@gmail.com>
2025-05-17 21:19:39 -0700

    Finish the Data Base of this system

--- src/main/java/com/university/sms/controller/Student_Controller.java
+++ src/main/java/com/university/sms/controller/Student_Controller.java
@@ -1,8 +1,14 @@
 package com.university.sms.controller;
 
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +24,26 @@ public class Student_Controller {
 
    @GetMapping
    public ResponseEntity<List<Student>> getStudents(){
-
     return new ResponseEntity<>(student_Service.getStudents(),HttpStatus.FOUND);
+   }
+
+   @PostMapping
+   public Student addStudent(@RequestBody Student student){
+      return student_Service.addStudent(student);
+   }
+
+   @PutMapping("/update/{id}")
+   public Student updatStudent(@RequestBody Student student,@PathVariable Long id){
+      return student_Service.updateStudent(student,id);
+   }
+
+   @DeleteMapping("/delete/{id}")
+   public void  deleteStudent(@PathVariable Long id){
+      student_Service.deleteStudent(id);
+   }
+
+   @GetMapping("/student/{id}")
+   public Student getStudentById(@PathVariable Long id){
+      return student_Service.getStudentById(id);
    }
 }
